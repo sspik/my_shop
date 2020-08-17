00cb350992249dcbabf7abc716c57d63591598e2
spik <sspik@mail.ru>
2020-08-17 13:46:11 +0300

    add image, status to product

--- Shop/models.py
+++ Shop/models.py
@@ -1,5 +1,22 @@
-from django.db import models
+import os
 import uuid
+
+from django.db.models.signals import pre_delete, pre_save
+from django.dispatch import receiver
+from django.db import models
+from django.conf import settings
+
+
+RECOMMEND = 'R'
+NEW = 'N'
+HIT = 'H'
+
+
+PRODUCT_STATUS = (
+    (RECOMMEND, 'Recommend'),
+    (NEW, 'New'),
+    (HIT, 'Hit'),
+)
 
 
 class PageModel(models.Model):
@@ -33,7 +50,7 @@ class Catalog(PageModel):
         null=True,
         blank=str
     )
-    image = models.FileField(upload_to='catalog/%Y/%m/%d/')
+    image = models.FileField(upload_to='upload/product/%Y/%m/%d/')
 
     def __str__(self):
         return self.name
@@ -51,14 +68,34 @@ class Product(PageModel):
         editable=False
     )
     name = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='upload/product/%Y/%m/%d/', null=True, blank=True)
     catalog = models.ForeignKey(
         Catalog,
         on_delete=models.SET_NULL,
         null=True,
     )
-    properties = models.JSONField(null=True, blank=True)
-    options = models.JSONField(null=True, blank=True)
+    quantity = models.DecimalField(max_digits=8, decimal_places=2, default=0)
+    product_status = models.CharField(
+        max_length=1,
+        choices=PRODUCT_STATUS,
+        null=True,
+        blank=True
+    )
     price = models.DecimalField(max_digits=8, decimal_places=2)
 
     def __str__(self):
         return f'{self.name} - {self.catalog.name}'
+
+
+@receiver(pre_delete, sender=Product)
+def delete_product_with_image(instance, **kwargs):
+    instance.image.delete()
+
+
+@receiver(pre_save, sender=Product)
+def delete_changed_image(instance, **kwargs):
+    current_product = Product.objects.get(id=instance.id)
+    if current_product.image and \
+            current_product.image != instance.image:
+        if os.path.isfile(current_product.image.path):
+            current_product.image.delete()
